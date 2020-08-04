42e91788ae1f5372ba37f4f083b16da30c7759a3
benittoraj <brendonbeni42@gmail.com>
2020-08-04 21:48:34 +0530

    route-parameter

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {BrowserRouter,Route} from 'react-router-dom'
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import Post from './components/Post';
 
 
 function App() {
@@ -17,6 +18,8 @@ function App() {
       <Route exact path='/' component={Home}></Route>
       <Route path='/about' component={About}></Route>
       <Route path='/contact' component={Contact}></Route>
+      <Route path='/:post_id' component={Post}></Route>
+
     </div>
     </BrowserRouter>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,19 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+class Home extends Component {
+  state = {
+    posts:[]
+  }
 
-const Home = ({ props }) => {
-    return (
-        <div className="container">
-  				<h4 className="center">Home</h4>
-  				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
-  				Aspernatur rerum dolor unde iure quisquam
-  				 tempore itaque, nemo inventore est, 
-  				 dignissimos tenetur quas cupiditate, obcaecati dolore 
-  				 fugit voluptatibus tempora? Ratione, ab.</p>
+  componentDidMount(){
+    axios.get("https://jsonplaceholder.typicode.com/posts").then(res => {
+
+      this.setState({
+        posts:res.data.slice(0,20)
+      })
+      // console.log(this.state.posts)
+    })
+
+  }
+    
+
+    render() {
+      const { posts } = this.state;
+      const postList = posts.length ? (
+        posts.map(post => {
+          return (
+            <div className="post card" key={post.id}>
+             <div className="card-content">
+             <div className="card-title">{post.title}</div>
+             <p>{post.body}</p>
+             </div>
             </div>
-
-    );
-};
-
-
+            )
+        })
+        ): (
+        <div className="center ">Loading...</div>);
+        return (
+            <div className="container">
+             {postList}
+            </div>
+        );
+    }
+}
 
 export default Home;
